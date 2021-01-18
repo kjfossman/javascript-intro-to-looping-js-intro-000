6ed3e4fc8ac75e4076ca5f290e4b319fd7978053
Kyle Fossman <kjfossman@gmail.com>
2021-01-18 22:36:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,7 +15,11 @@ function whileLoop(n){
   n --}
   return "done"
 }
-
+let i = 0
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
 function doWhileLoop(num){
   do{
     console.log("I run once regardless");
